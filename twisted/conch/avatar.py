e7a04af05e5d281b1d73774a95f0e406a61c142e
Einar Floystad Dorum <edorum@cisco.com>
2016-07-21 17:21:19 +0200

    Get forwarding and general conch.ssh passing on Python3

--- twisted/conch/avatar.py
+++ twisted/conch/avatar.py
@@ -6,6 +6,7 @@ from twisted.conch.error import ConchError
 from twisted.conch.interfaces import IConchUser
 from twisted.conch.ssh.connection import OPEN_UNKNOWN_CHANNEL_TYPE
 from twisted.python import log
+from twisted.python.compat import nativeString
 
 
 @implementer(IConchUser)
@@ -32,7 +33,7 @@ class ConchUser:
 
     def gotGlobalRequest(self, requestType, data):
         # XXX should this use method dispatch?
-        requestType = requestType.replace('-', '_')
+        requestType = nativeString(requestType.replace(b'-', b'_'))
         f = getattr(self, "global_%s" % requestType, None)
         if not f:
             return 0
